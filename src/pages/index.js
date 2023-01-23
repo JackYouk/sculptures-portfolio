b3c055cc69be195a067d60782fcba3bcae7f7d8b
JackYouk <jackyoukstetter1@gmail.com>
2023-01-23 01:37:12 -0800

    added shoutout

--- src/pages/index.js
+++ src/pages/index.js
@@ -302,7 +302,7 @@ export default function ComputerInception3D() {
               <br />
               <br />
               To ever-so-humbly introduce myself,
-              I'm a creative and a "Jack of all Trades" software engineer,
+              I am a creative and a "Jack of all Trades" software engineer,
               with both jaw-dropping and mesmerizing web development
               abilities.
               <br />
@@ -379,7 +379,7 @@ export default function ComputerInception3D() {
           <section id='credits'>
             <h2>Credits</h2>
             <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+              Shout out to @Fragmaster on Thingiverse and Fallout 4 bobblehead design team for the 3d models used in this project.
             </p>
             <a href='#top' style={{ fontSize: 'large' }}>Top</a>
           </section>
